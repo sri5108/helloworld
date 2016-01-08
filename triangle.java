dece006bc0891670e5af4359f572986ea211ef31
sri5108 <schaitanya325@gmail.com>
2016-01-08 13:16:31 +0530

    i added a new file

---
+++ triangle.java
@@ -0,0 +1,19 @@
+
+public class Traingle{ 
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+        
+       for(int i=1;i<=4;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+    }
+    
+}
+
+}
